2acb177ed134ea6ae32aa577706082f41a197c06
Min Thiha Tun <mtht1794@gmail.com>
2025-01-13 00:26:59 +0630

    fix_forgetting__get_random_text_function_arguments

--- app.py
+++ app.py
@@ -59,7 +59,7 @@ with col1:
 
 with col2:
     if st.button("Get test data"):
-        st.session_state.user_input = get_random_text()
+        st.session_state.user_input = get_random_text(st.session_state.user_input)
         st.toast("Test data loaded into text area.")
 
 #<-------------- app end ------------------------>
